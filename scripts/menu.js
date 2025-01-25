c8f08f0d2d24762578723a1c47b684a0b9b3c50a
Graystevo <grayrules@yahoo.com>
2025-01-24 22:21:08 -0700

    cleaning up formatting

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,20 @@
-
-
 const menuButton = document.querySelector(".header-nav #menu");
 const menu = document.querySelector(".header-nav .navigation");
 
 // Toggle menu on button click
 menuButton.addEventListener("click", () => {
-    menu.classList.toggle("open");
-    menuButton.classList.toggle("open")
+  menu.classList.toggle("open");
+  menuButton.classList.toggle("open");
 });
 
 function handleResize() {
   const menu = document.querySelector(".navigation");
-  if (window.innerWidth > 840) {
+  if (window.innerWidth >= 840) {
     menu.classList.add("open");
   } else {
     menu.classList.remove("open");
   }
 }
-
 
 handleResize();
 window.addEventListener("resize", handleResize);
