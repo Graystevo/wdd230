a66843458b1d7525ae87e23e6269382c223bceb6
Graystevo <grayrules@yahoo.com>
2025-02-08 16:39:30 -0700

    dark mode toggle written and fit to page

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -18,3 +18,19 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+// the stuff for dark mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌑")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🌑";
+	}
+});
