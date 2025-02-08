0b9c1df32f91bab3a0174d7e1712f0cbc01d5ff4
Graystevo <grayrules@yahoo.com>
2025-02-08 15:07:09 -0700

    switched to arrow function

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -2,7 +2,8 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {  // Add your code here for what happens when the button is clicked
+button.addEventListener('click', () => {
+    // Add your code here for what happens when the button is clicked
     const chapter = input.value; // Get the value from the input
     if (chapter) {
         const listItem = document.createElement('li'); // Create a new list item
@@ -11,5 +12,3 @@ button.addEventListener('click', function() {  // Add your code here for what ha
         input.value = ''; // Clear the input field after adding the chapter
     }
 });
-
-button.addEventListener('click', () => { ... });
