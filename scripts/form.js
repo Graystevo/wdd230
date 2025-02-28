4bb95b33bf1a4324167ca08a9363fcce34246fef
Graystevo <grayrules@yahoo.com>
2025-02-27 17:31:19 -0700

    finished form

--- scripts/form.js
+++ scripts/form.js
@@ -28,23 +28,3 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-	// Function to handle form submission
-	function handleSubmit(event) {
-		// Prevent default form submission
-		event.preventDefault();
-
-		// Access form elements
-		let formt = event.target;
-		let formData = new FormData(formt);
-
-		// Display form element values
-		for (let pair of formData.entries()) {
-			console.log(pair[0] + ": " + pair[1]);
-		}
-	}
-
-	const form = document.querySelector("form");
-	form.addEventListener("submit", handleSubmit);
-});
