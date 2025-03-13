219978b810f4b64e0b5cfdc9f3640c184961bad2
Graystevo <grayrules@yahoo.com>
2025-03-13 16:24:02 -0600

    weather.html and js created and tested

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,36 @@
+// 543b25f5306a6bd35ae1625b04e51bca
+// TRIER DE: [49.75, 6.63]
+// ST GEORGE:  [37.1042, -113.5841]
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=543b25f5306a6bd35ae1625b04e51bca&units=imperial';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('src', iconsrc);
+    captionDesc.textContent = `${desc}`;
+  }
+  
+  apiFetch();
