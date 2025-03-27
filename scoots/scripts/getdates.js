264cf5e5429fc1997c0bcae5b8ebdbf8fbf640f5
Graystevo <grayrules@yahoo.com>
2025-03-26 19:41:56 -0600

    made dark mode better. removed color from getdates last edit line

--- scoots/scripts/getdates.js
+++ scoots/scripts/getdates.js
@@ -7,6 +7,5 @@ window.onload = function() {
     
     if (footerParagraphs.length >= 3) {
         footerParagraphs[3].textContent = `Last updated: ${lastModified}`;
-        footerParagraphs[3].style.color = "rgb(80, 80, 1)";
     }
 };
--- scoots/scripts/menu.js
+++ scoots/scripts/menu.js
@@ -21,16 +21,16 @@ window.addEventListener("resize", handleResize);
 
 // the stuff for dark mode
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌑")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
+		body.style.background = "#666";
+		body.style.color = "#fff";
 		modeButton.textContent = "🔆";
 	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
+		body.style.background = "#eee";
+		body.style.color = "#666";
 		modeButton.textContent = "🌑";
 	}
 });
